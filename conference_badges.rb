89e8fe7ba81378dcca941cf1799184cfbcab2b98
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-02-04 12:40:25 -0500

    commit tree cf55ddc0e57d45a75a2607e94a8c69ea49a39b30

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+require "pry"
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    speakers = []
+    #binding.pry
+    attendees.each do |name|
+        speakers << "Hello, my name is #{name}."
+        end
+speakers
+end
+
+def assign_rooms(attendees)
+    room_assignments = []
+    attendees.each.with_index(1) do |name, assign|
+        room_assignments << "Hello, #{name}! You'll be assigned to room #{assign}!"
+    end
+room_assignments
+end
+
+def printer(name)
+    batch_badge_creator(name).each do |blank|
+        puts blank
+    end
+    assign_rooms(name).each do |blank|
+        puts blank
+    end
+end
+
